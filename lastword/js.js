eb38cb1d80e860434bf8a6487b773ae1779834d4
Charley1013 <79081800+jiho3894@users.noreply.github.com>
2021-09-27 21:11:34 +0900

    Add files via upload

--- lastword/js.js
+++ lastword/js.js
@@ -1,36 +1,44 @@
 const input = document.querySelector("input");
-const button = document.querySelector("button");
 const span = document.querySelector("span");
 const num = document.querySelector(".num");
 let word;
 let newWord;
+setNum = 1;
+
+const gameLength = parseInt(prompt("쿵쿵따 길이 설정"));
+const gameMember = parseInt(prompt("인원수 설정"));
+console.log(typeof(gameMember));
+
+(isNaN(gameLength) || isNaN(gameMember)) ? 
+location.reload(alert("숫자만 입력해주세요")) : 
+alert("게임을 시작합니다!");
 
 const control = () => {
   word = newWord;
   span.textContent = word;
   input.value = '';
-  number= 1;
-  number = number + 1;
-  num.innerHTML = number;
   input.focus();
+  setNum = setNum + 1;
 }
 
-const onClick = () => {
-  if(!word || word[word.length - 1] === newWord[0]) {
+const onPress = (e) => {
+  if(e.keyCode == 13) {
+  if(newWord.length === gameLength && (!word || word[word.length - 1] === newWord[0])) {
     control();
+    num.textContent = `${setNum}번째 참가자`;
+    if(setNum >= gameMember) {
+      return setNum = 0;
+    }
   } else {
-      alert("오류");
+      alert(`${gameLength}자리만 입력해주세요`);
       input.value = '';
-      num.innerHTML = number;
     }
   }
+}
 
 const inputWord = (e) => {
   newWord = e.target.value;
-  if(newWord.length === 0) {
-    return undefined;
-  }
 }
 
-button.addEventListener('click' , onClick);
 input.addEventListener('input' , inputWord);
+input.addEventListener('keypress', onPress);
