1989532cf0b7d38a8d423af11046f06c5771b65e
Charley1013 <79081800+jiho3894@users.noreply.github.com>
2021-10-09 23:25:05 +0900

    Add files via upload

--- RamdomNumber/index.js
+++ RamdomNumber/index.js
@@ -1,16 +1,19 @@
 const h1 = document.querySelector("h1");
+const p = document.querySelector("p");
 
-const candidate = Array(45).fill().map((e,i) => i + 1);
+const num = Array(45).fill().map((_,i) => i + 1);
 const shuffle = [];
 
-while(candidate.length > 0) {
-  const random = Math.floor(Math.random() * candidate.length);
-  const spliceArray = candidate.splice(random,1);
+for (let i = 0; i < num.length; i++) {
+  const random = Math.floor(Math.random() * num.length);
+  const spliceArray = num.splice(random,1);
   const value = spliceArray[0];
   shuffle.push(value);
 }
 
-const winBalls = shuffle.slice(0,6).sort((a,b) => a - b);
+const numPick = shuffle.slice(0,6).sort((a,b) => a - b);
 const bonus = shuffle[6];
-console.log(winBalls);
-console.log(bonus);
+
+h1.textContent = `심심해 : ${numPick}`;
+p.textContent = `놀아줘 : ${bonus}`;
+
